a828e8c171412d780a433dbed6a575bdf9dc3d22
RufusTenali <akil.tenali.2019@mumail.ie>
2022-03-23 21:33:25 +0000

    info cards updated

--- my-app/src/pages/info.js
+++ my-app/src/pages/info.js
@@ -10,37 +10,51 @@ const Info = () => {
         <h2 id="text">What is Dementia?</h2>
       </div>
       <div class="flip-box-back">
-        <h2>Play</h2>
+        <h2>Dementia is the loss of cognitive functions such as remembering, thinking, speaking to an extent that interferes with the person’s everyday life.
+        </h2>
+        <h2><a href="https://www.mayoclinic.org/diseases-conditions/dementia/symptoms-causes/syc-20352013" target="_blank">Click Here</a></h2>
       </div>
     </div>
   </div>
   <div id="info" class="flip-box">
     <div class="flip-box-inner">
       <div class="flip-box-front">
-        <h2>Information 2</h2>
+        <h2>What casues Dementia?</h2>
       </div>
       <div class="flip-box-back">
-        <h2>Play</h2>
+        <h2>Dementia is the term to describe a group of conditions that cause damage to the brain cells. Dementia is caused by damage to or loss of nerve cells and their connections in the brain</h2>
+        <h2>
+        <a href="https://www.youtube.com/watch?v=HobxLbPhrMc" target="_blank">Click Here</a>
+        </h2>
       </div>
     </div>
   </div>
   <div id="info" class="flip-box">
     <div class="flip-box-inner">
       <div class="flip-box-front">
-        <h2>Information 3</h2>
+        <h2>5 Steps you can take to reduce risk of dementia</h2>
       </div>
       <div class="flip-box-back">
-        <h2>Play</h2>
+        <a href="https://www.youtube.com/watch?v=HobxLbPhrMc" target="_blank">1.	Be Physically Active
+        <br></br>
+2.	Eat a Healthy Diet
+<br></br>
+3.	Take care of your heart
+<br></br>
+4.	Mentally challenge yourself
+<br></br>
+5.	Be social engaged
+</a>
       </div>
     </div>
   </div>
   <div id="info" class="flip-box">
     <div class="flip-box-inner">
       <div class="flip-box-front">
-        <h2>Information 4</h2>
+        <h2>What else can you do?</h2>
       </div>
       <div class="flip-box-back">
-        <h2>Play</h2>
+        <h2>Contact a medical professional for further assistance</h2>
       </div>
     </div>
   </div>
